0ba6e8cf5e242603214d5284853e7dedd23cda1f
Bruno6244bh <bruno6244.bh@gmail.com>
2023-03-23 08:56:57 -0300

    script changes and visual sttings

--- script.js
+++ script.js
@@ -7,93 +7,77 @@ var playerPoints = 0;
 function chooseRock () {
     playerChoice = "rock";
     console.log("Player chose: " + playerChoice);
+    document.getElementById("rockButton").style.backgroundColor = "#023C40";
+    document.getElementById("paperButton").style.backgroundColor = "white";
+    document.getElementById("scissorButton").style.backgroundColor = "white";
 }
 
 function choosePaper () {
     playerChoice = "paper";
     console.log("Player chose: " + playerChoice);
+    document.getElementById("rockButton").style.backgroundColor = "white";
+    document.getElementById("paperButton").style.backgroundColor = "#023C40";
+    document.getElementById("scissorButton").style.backgroundColor = "white";
 }
 
 function chooseScissor () {
     playerChoice = "scissor";
     console.log("Player chose: " + playerChoice);
+    document.getElementById("rockButton").style.backgroundColor = "white";
+    document.getElementById("paperButton").style.backgroundColor = "white";
+    document.getElementById("scissorButton").style.backgroundColor = "#023C40";
 }
 
 function randomNumber(a,b) {
-
     randomGenerator = Math.floor(Math.random() * (b - a + 1)) + a;
     return randomGenerator;
-    
 }
 
 function switchPcChoice (randomGenerator) {
-    if (randomGenerator == 0) {
+    if (randomGenerator === 0) {
         pcChoice = "rock";
         console.log("pc choose: " + pcChoice);
-    } else if (randomGenerator == 1) {
+    } else if (randomGenerator === 1) {
         pcChoice = "paper";
         console.log("pc choose: " + pcChoice);
-    } else if (randomGenerator == 2) {
+    } else if (randomGenerator === 2) {
         pcChoice = "scissor";
         console.log("pc choose: " + pcChoice);
     }
 }
 
 function comparation (playerChoice, pcChoice) {
-
-    switch (playerChoice, pcChoice) {
-
-        case ("rock", "scissor"):
-            playerPoints +1;
-            // console.log ("player:" + playerPoints / "pc:" + pcPoints );
-        break;
-
-        case ("rock", "paper"):
-            pcPoints +1;
-            // console.log ("player:" + playerPoints / "pc:" + pcPoints );
-        break;
-
-        case ("rock", "rock"):
-            // console.log ("player:" + playerPoints / "pc:" + pcPoints );
-        break;
-
-        case ("paper", "scissor"):
-            pcPoints +1;
-            // console.log ("player:" + playerPoints / "pc:" + pcPoints );
-        break;
-
-        case ("paper", "paper"):
-            // console.log ("player:" + playerPoints / "pc:" + pcPoints );
-        break;
-
-        case ("paper", "rock"):
-            playerPoints +1;
-            // console.log ("player:" + playerPoints / "pc:" + pcPoints);
-        break;
-
-        case ("scissor", "scissor"):
-            // console.log ("player:" + playerPoints / "pc:" + pcPoints );
-        break;
-
-        case ("scissor", "paper"):
-            playerPoints +1;
-            // console.log ("player:" + playerPoints / "pc:" + pcPoints );
-        break;
-
-        case ("scissor", "rock"):
-            pcPoints +1;
-            // console.log ("player:" + playerPoints / "pc:" + pcPoints );
-        break;
-
+    if (playerChoice==="rock" & pcChoice==="scissor" | playerChoice==="paper" & pcChoice==="rock" | playerChoice==="scissor" & pcChoice==="paper" ) {
+        playerPoints++;
+        console.log("You Win!")
+        document.getElementById("resultOutput").innerHTML = `Computer chooses  ${pcChoice}, You win!`;
+    } else if (playerChoice==="rock" & pcChoice==="paper" | playerChoice==="paper" & pcChoice==="scissor" | playerChoice==="scissor" & pcChoice==="rock" ) {
+        pcPoints++;
+        document.getElementById("resultOutput").innerHTML = `Computer chooses  ${pcChoice}, You Lose!`;
+    } else {
+        document.getElementById("resultOutput").innerHTML = `Computer chooses  ${pcChoice}, It's a draw!`;
     }
 }
 
-
+function cleanRound () {
+    playerChoice = undefined;
+    document.getElementById("rockButton").style.backgroundColor = "white";
+    document.getElementById("paperButton").style.backgroundColor = "white";
+    document.getElementById("scissorButton").style.backgroundColor = "white";
+}
 
 function gamePosSubmit () {
+
+    if (playerChoice === undefined) {
+        alert ("Choose one option!")
+        document.getElementById("resultOutput").innerHTML = ` `;
+    } else {
     randomNumber (0,2);
     switchPcChoice (randomGenerator);
     comparation (playerChoice, pcChoice)
     console.log ("vc: " + playerPoints);
     console.log ("pc: " + pcPoints);
+    cleanRound();
+    }
+    
 }
